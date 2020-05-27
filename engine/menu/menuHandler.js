498d025821be56e8eaee16fea0072b7d9bd67f06
Essentuan <51426319+Essentuan@users.noreply.github.com>
2020-05-27 01:28:16 -0500

    Update menuHandler.js

--- engine/menu/menuHandler.js
+++ engine/menu/menuHandler.js
@@ -1,5 +1,4 @@
-let CSSURL = window.location.href + 'libraries/three.js/CSS3DRenderer.js'
-import {CSS3DRenderer, CSS3DObject} from CSSURL;
+import {CSS3DRenderer, CSS3DObject} from '/./libraries/three.js/CSS3DRenderer.js';
 menu.components.CSS3DRenderer = CSS3DRenderer
 menu.components.CSS3DObject = CSS3DObject
 menu.loadedElements.pause = $.loadHTML("./engine/menu/pause.html", false)
